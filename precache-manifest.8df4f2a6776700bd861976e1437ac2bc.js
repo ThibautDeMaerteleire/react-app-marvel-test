7f0fb694aa1bb34f0ce5a052c8823b7df5440381
Codelicious <thibdema@student.arteveldehs.be>
2020-05-10 15:22:31 +0200

    lol

---
+++ precache-manifest.8df4f2a6776700bd861976e1437ac2bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904d34c21ba4acc6a92169f7b593546d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3109f6990ddd688c2d7a",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0661cb118e3eb947c163",
+    "url": "/static/css/main.a871de15.chunk.css"
+  },
+  {
+    "revision": "3109f6990ddd688c2d7a",
+    "url": "/static/js/2.158dd191.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.158dd191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0661cb118e3eb947c163",
+    "url": "/static/js/main.58e2a125.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
