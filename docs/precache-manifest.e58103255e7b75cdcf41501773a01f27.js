d7d20698bfba3e43c8c58dee63939340a2139f86
Codelicious <thibdema@student.arteveldehs.be>
2020-05-11 09:40:15 +0200

    added pages

---
+++ docs/precache-manifest.e58103255e7b75cdcf41501773a01f27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f894fb24f12910715b019cebc96dd7d",
+    "url": "/react-app-marvel-test/index.html"
+  },
+  {
+    "revision": "3109f6990ddd688c2d7a",
+    "url": "/react-app-marvel-test/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "446be34f43b35f4f024f",
+    "url": "/react-app-marvel-test/static/css/main.a871de15.chunk.css"
+  },
+  {
+    "revision": "3109f6990ddd688c2d7a",
+    "url": "/react-app-marvel-test/static/js/2.158dd191.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-marvel-test/static/js/2.158dd191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446be34f43b35f4f024f",
+    "url": "/react-app-marvel-test/static/js/main.9fa67732.chunk.js"
+  },
+  {
+    "revision": "73b00dcafc3fa4a18ac2",
+    "url": "/react-app-marvel-test/static/js/runtime-main.8e51d87f.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8df4f2a6776700bd861976e1437ac2bc.js"
+  "/react-app-marvel-test/precache-manifest.e58103255e7b75cdcf41501773a01f27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-marvel-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
